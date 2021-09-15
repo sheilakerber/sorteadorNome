5077e065eb692cbf7c12e4dd144977ee612e5d46
Sheila Kerber <sheila.skerber@gmail.com>
2021-09-15 09:29:17 -0300

    primeiro commit

---
+++ sorteadorNome/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSortear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNome1 = New System.Windows.Forms.TextBox()
+        Me.txtNome3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNome2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtNome4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNome5 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSortear
+        '
+        Me.btnSortear.Location = New System.Drawing.Point(42, 178)
+        Me.btnSortear.Name = "btnSortear"
+        Me.btnSortear.Size = New System.Drawing.Size(189, 23)
+        Me.btnSortear.TabIndex = 0
+        Me.btnSortear.Text = "Sortear"
+        Me.btnSortear.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(41, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nome 1:"
+        '
+        'txtNome1
+        '
+        Me.txtNome1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome1.Location = New System.Drawing.Point(105, 32)
+        Me.txtNome1.Name = "txtNome1"
+        Me.txtNome1.Size = New System.Drawing.Size(126, 21)
+        Me.txtNome1.TabIndex = 2
+        '
+        'txtNome3
+        '
+        Me.txtNome3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome3.Location = New System.Drawing.Point(105, 87)
+        Me.txtNome3.Name = "txtNome3"
+        Me.txtNome3.Size = New System.Drawing.Size(126, 21)
+        Me.txtNome3.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(39, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nome 3:"
+        '
+        'txtNome2
+        '
+        Me.txtNome2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome2.Location = New System.Drawing.Point(105, 59)
+        Me.txtNome2.Name = "txtNome2"
+        Me.txtNome2.Size = New System.Drawing.Size(126, 21)
+        Me.txtNome2.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(41, 60)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 16)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Nome 2:"
+        '
+        'txtNome4
+        '
+        Me.txtNome4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome4.Location = New System.Drawing.Point(105, 114)
+        Me.txtNome4.Name = "txtNome4"
+        Me.txtNome4.Size = New System.Drawing.Size(126, 21)
+        Me.txtNome4.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(39, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Nome 4:"
+        '
+        'txtNome5
+        '
+        Me.txtNome5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome5.Location = New System.Drawing.Point(105, 141)
+        Me.txtNome5.Name = "txtNome5"
+        Me.txtNome5.Size = New System.Drawing.Size(126, 21)
+        Me.txtNome5.TabIndex = 14
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(39, 141)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 16)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Nome 5:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(276, 236)
+        Me.Controls.Add(Me.txtNome5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNome4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNome2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtNome3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNome1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSortear)
+        Me.Name = "Form1"
+        Me.Text = "Sorteador"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSortear As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNome1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNome3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNome2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtNome4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtNome5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ sorteadorNome/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSortear.Click
+        ' valida preenchimento dos campos
+        If (txtNome1.Text = Nothing Or txtNome2.Text = Nothing Or txtNome3.Text = Nothing Or txtNome4.Text = Nothing Or txtNome5.Text = Nothing) Then
+            MsgBox("Há campos vazios. Por favor, verifique.", MsgBoxStyle.Critical, "Atenção!")
+            Exit Sub
+        End If
+
+        ' se tudo estiver preenchido, cria o array nomes e guarda os inputs do usuario
+        Dim nomes As Object = {txtNome1.Text, txtNome2.Text, txtNome3.Text, txtNome4.Text, txtNome5.Text}
+
+        ' sorteia um index do array
+        Dim nomeSorteado As Integer = CInt(Int(5 * Rnd() + 1))
+
+        ' apresenta na tela o nome correspondente ao index sorteado
+        MsgBox("Nome sorteado: " & nomes(nomeSorteado))
+
+        'limpa os campos
+        txtNome1.Text = Nothing
+        txtNome2.Text = Nothing
+        txtNome3.Text = Nothing
+        txtNome4.Text = Nothing
+        txtNome5.Text = Nothing
+    End Sub
+End Class
